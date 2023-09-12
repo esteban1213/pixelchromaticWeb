0bf4677e66be1eff2f75e4da7f47477dd3e724e8
Esteban Aguirre Iglesias <estebanaguirreiglesias@Estebans-Air.frontiernet.net>
2023-09-11 20:45:34 -0700

    conditional document

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,20 @@
  // This is a client component 👈🏽
  import { BrowserRouter  } from "react-router-dom";
  import Home from "./index"; // Make sure this is the correct import path
+ import { useEffect } from "react";
 
  
  // ...
  
  export default function App() {
+  useEffect(() => {
+    if (typeof document !== 'undefined') {
+      // Your client-side code here
+    }
+  }, []);
    return (
      
        <BrowserRouter>
-      
          <Home />
        </BrowserRouter>
     
