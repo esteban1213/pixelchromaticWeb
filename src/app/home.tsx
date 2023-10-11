d68bcc3400ea3def3e283b281d616d3749ec19ca
Esteban Aguirre Iglesias <estebanaguirreiglesias@Estebans-MacBook-Air.local>
2023-10-11 11:39:27 -0700

    Version 4.1

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -10,11 +10,12 @@ import {
 } from "react-icons/bi";
 
 // IMAGES
-import Logo from "../../public/images/logo.png";
+import Logo from "../../public/icons/logo.png";
 import bulb from "../../public/icons/bulb.png";
 import rocket from "../../public/icons/rocket.png";
 import tap from "../../public/icons/tap.png";
 import heart from "../../public/icons/heart.png";
+import logoWhite from "../../public/icons/logowhite.png";
 
 // ANIMATION VARIANTS
 import { fadeUp, mainFadeVariant, swipeOut } from "./animations";
@@ -51,49 +52,47 @@ export default function Home() {
           Boost Your Success With Unique & Intuitive Solutions!
         </div>
       </motion.div>
-      <div className={styles.visionCards}>
-        <div className={styles.visionCont}>
-          <Image src={heart} alt={"bulb"} className={styles.icons} />
-          <p className={styles.cardTitle}>What we believe in.</p>
-          <p className={styles.cardDescription}>
-            At Pixelchromatic, we believe in the transformative power of
-            intuitive technology and digital tools that drive success. We are
-            committed to creating innovative, intuitive solutions, both large
-            and small, that address real-world problems and empower people and
-            organizations to reach their full potential.{" "}
-          </p>
-        </div>
-        <br />
-        <div className={styles.visionCont}>
-          <Image src={bulb} alt={"bulb"} className={styles.icons} />
-          <p className={styles.cardTitle}>It all starts with your vision</p>
-          <p className={styles.cardDescription}>
-            We want to make sure we create solutions that align with your goals.
-            In a world saturated by templates and tools made for the masses, you
-            never get the tools that truly resonate with YOU.
-          </p>
-        </div>
-        <br />
-        <div className={styles.visionCont}>
-          <Image src={rocket} alt={"rocket"} className={styles.icons} />
-          <p className={styles.cardTitle}>Don't get stuck</p>
-          <p className={styles.cardDescription}>
-            Software is dynamic. In an ever changing world full of trends and
-            innovation. You don't have to be stuck. Your solutions are always
-            backed by the versatilty of custom software.
-          </p>
-        </div>
-        <br />
-        <div className={styles.visionCont}>
-          <Image src={tap} alt={"tap"} className={styles.icons} />
-          <p className={styles.cardTitle}>It's Intuitive</p>
-          <p className={styles.cardDescription}>
-            Whatever your goal, our tailored approach guarantees solutions that
-            are both intuitive and scalable. Every user will find our offerings
-            familiar yet prefectly unique.
-          </p>
-        </div>
+      <div className={`${styles.visionCont} ${styles.visionContOne}`}>
+        <Image src={heart} alt={"bulb"} className={styles.icons} />
+        <p className={styles.cardTitle}>What we believe in.</p>
+        <p className={styles.cardDescription}>
+          At Pixelchromatic, we believe in the transformative power of intuitive
+          technology and digital tools that drive success. We are committed to
+          creating innovative, intuitive solutions, that address real-world
+          problems and empower people and organizations to reach their full
+          potential. <br /> <br /> More coming soon...
+        </p>
       </div>
+      <br />
+      {/* <div className={styles.visionCont}>
+        <Image src={bulb} alt={"bulb"} className={styles.icons} />
+        <p className={styles.cardTitle}>It all starts with your vision</p>
+        <p className={styles.cardDescription}>
+          We want to make sure we create solutions that align with your goals.
+          In a world saturated by templates and tools made for the masses, you
+          never get the tools that truly resonate with YOU.
+        </p>
+      </div>
+      <br />
+      <div className={styles.visionCont}>
+        <Image src={rocket} alt={"rocket"} className={styles.icons} />
+        <p className={styles.cardTitle}>Don't get stuck</p>
+        <p className={styles.cardDescription}>
+          Software is dynamic. In an ever changing world full of trends and
+          innovation. You don't have to be stuck. Your solutions are always
+          backed by the versatilty of custom software.
+        </p>
+      </div>
+      <br />
+      <div className={styles.visionCont}>
+        <Image src={tap} alt={"tap"} className={styles.icons} />
+        <p className={styles.cardTitle}>It's Intuitive</p>
+        <p className={styles.cardDescription}>
+          Whatever your goal, our tailored approach guarantees solutions that
+          are both intuitive and scalable. Every user will find our offerings
+          familiar yet prefectly unique.
+        </p>
+      </div> */}
       <br />
       <br />
     </motion.main>
